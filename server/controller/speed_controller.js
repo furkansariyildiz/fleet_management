c51099e063f401940bcb78daf3dd8754d23b70b2
furkansariyildiz <furkansariyildiz@windowslive.com>
2022-12-06 17:57:36 +0300

    CmdVelPublisher fonksiyonu yazildi, PostMan ile test edildi. 'Speed' Topic'i ile '/cmd_vel' publish edilmektedir.

--- server/controller/speed_controller.js
+++ server/controller/speed_controller.js
@@ -1,9 +1,13 @@
 const publishCmdVel = require('../scripts/utils/speed/publish_cmd_vel').publishCmdVel;
-const speed_topics = require('../scripts/utils/ros_connections/ros_connections');
+const arrayFunctions = require('../scripts/utils/array_functions/array_functions');
+const findRobot = require('../scripts/utils/robots/find_robots').findRobots;
+const speed_topics = require('../scripts/utils/ros_connections/ros_initialize').speed_topics;
 
 
-function speedController(connection, received_message, io){
-    publishCmdVel()
+async function speedController(connection, received_message, io){
+    var robot = await findRobot({id: received_message.robotId});
+    var index_of_speed_topic = arrayFunctions.findIndex(speed_topics, robot[0].url);
+    publishCmdVel(speed_topics[index_of_speed_topic][robot[0].url].cmd_vel, 0.1, 0.1);
 };
 
 
--- server/routes/route_manager.js
+++ server/routes/route_manager.js
@@ -1,6 +1,6 @@
 const indexRouter = require('./index_route').indexRouter;
 const resetRobotRouter = require('./reset_robot_route').resetRobotRouter;
-
+const speedRouter = require('./speed_route').speedRouter;
 
 module.exports.socketIORouteManager = function(io){
     io.on('connection', function(socket){
@@ -12,6 +12,10 @@ module.exports.socketIORouteManager = function(io){
 
         socket.on('ResetRobot', (received_message) => {
             resetRobotRouter('ResetRobot', received_message, io);
+        });
+
+        socket.on('Speed', (received_message) => {
+            speedRouter('Speed', received_message, io);
         });
     });
 };
